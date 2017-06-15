9ba00f5ac9dbae982830fb2cf7209d5d95c8920b
Pablo Magaz <pmagaz@atsistemas.com>
2017-06-15 14:42:34 +0200

    Update templates

--- server/templates/index.js
+++ server/templates/index.js
@@ -1,4 +1,27 @@
-import main from './main';
-import calculator from './calculator';
+import base from 'base';
+import getScripts from '../lib/GetScripts';
 
-export { main, calculator };
+export default function renderPage() {
+
+  const app = getScripts('app');
+  const vendor = getScripts('vendor');
+  const style = (base.env === 'production') ? '<link rel="stylesheet" href="bundle.css">' : '';
+
+  return `
+  <!doctype html>
+	<html lang="utf-8">
+    <head>
+      <title>Angular2 Base</title>
+      <base href="/">
+      ${ style }
+      ${ vendor }
+    </head>
+    <body>
+    <div id="app">
+    </div>
+    ${ app }
+    </body>
+  </html>
+  `;
+
+}
