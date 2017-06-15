894abc82a177cf6977ec0459c1ee4022373089c8
Pablo Magaz <pmagaz@atsistemas.com>
2017-06-15 14:42:20 +0200

    Update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,3 @@
-// http://eslint.org/docs/user-guide/configuring
-
 module.exports = {
   root: true,
   parser: 'babel-eslint',
@@ -10,30 +8,33 @@ module.exports = {
     browser: true,
   },
   extends: 'airbnb-base',
-  // required to lint *.vue files
   plugins: [
     'html'
   ],
-  // check if imports actually resolve
-  'settings': {
-    'import/resolver': {
-      'webpack': {
-        'config': 'build/webpack.base.conf.js'
-      }
-    }
-  },
-  // add your custom rules here
   'rules': {
-    // don't require .vue extension when importing
     'import/extensions': ['error', 'always', {
       'js': 'never',
       'vue': 'never'
     }],
-    // allow optionalDependencies
-    'import/no-extraneous-dependencies': ['error', {
-      'optionalDependencies': ['test/unit/index.js']
-    }],
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'import/no-extraneous-dependencies': 0,
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    "import/no-duplicates": 0,
+    "import/no-extraneous-dependencies": 0,
+    "import/extensions": 0,
+    "import/named": 0,
+    "import/namespace": 0,
+    "import/no-unresolved": 0,
+    "import/no-named-as-default": 0,
+    "import/prefer-default-export": 0,
+    "comma-dangle": 0,
+    "indent": [2, 2, {"SwitchCase": 1}],
+    "no-console": 0,
+    "no-alert": 0,
+    "semi": 2,
+    "no-var": 2,
+    "space-before-blocks": 2,
+    "keyword-spacing": 2,
+    "no-global-assign": 0,
+    "no-unsafe-negation": 0
   }
 }
