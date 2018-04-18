37925ab8898d8a58a3ccf3d46f16f50f23461cd1
Jose Manuel Bermudo <josemanuelbd@gmail.com>
2018-04-18 18:08:39 +0200

    added lazy unit files

--- src/app/containers/home/home.container.spec.js
+++ src/app/containers/home/home.container.spec.js
@@ -1,18 +1,17 @@
-import { expect } from 'chai'
 import { mount } from '@vue/test-utils'
 import HomeContainer from './home.container.vue'
 
 describe('HomeContainer', () => {
   it('sets the correct name', () => {
-    expect(typeof HomeContainer.name).to.be.a('string')
+    expect(typeof HomeContainer.name).toEqual('string')
     const name = HomeContainer.name
-    expect(name).to.be.equal('HomeMain')
+    expect(name).toEqual('HomeMain')
   })
 
   // Mount an instance and inspect the render output
   it('renders the correct logo', () => {
     const wrapper = mount(HomeContainer)
 
-    expect(wrapper.contains('img')).to.be.equal(true)
+    expect(wrapper.contains('img')).toEqual(true)
   })
 })
--- src/base/index.js
+++ src/base/index.js
@@ -5,8 +5,6 @@ export { env }
 export { console }
 export default { console, env }
 export { typeBuilder } from './shared/TypeHelper'
-export { actionCreator } from './shared/ActionCreator'
-export { createReducer } from './shared/CreateReducer'
 export { generateFetchTypes } from './shared/TypeHelper'
 export { fetchRequiredActions } from './shared/FetchData'
 export { createActionType } from './shared/createActionType'
