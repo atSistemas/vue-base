eea9e71f68ddc4012cdac6d4237a68b64bec07fa
Jose Manuel Bermudo <josemanuelbd@gmail.com>
2018-04-19 11:01:06 +0200

    refactor removed changes

--- src/base/shared/spec/FileSystem.shared.spec.js
+++ src/base/shared/spec/FileSystem.shared.spec.js
@@ -1,64 +1,49 @@
-import fs from 'fs';
-import path from 'path';
-import { expect } from 'chai';
-import { fileExists, readDir, writeFile, readFile } from '../FileSystem';
+import fs from 'fs'
+import path from 'path'
+import { fileExists, readDir, writeFile, readFile } from '../FileSystem'
 
 describe('shared / FileSystem', () => {
-
   describe('fileExists', () => {
-
     it('Sould check if fileExist', () => {
+      const realPath = path.resolve(__dirname, '..', 'FileSystem.js')
+      const fakePath = path.resolve(__dirname, '..', 'fake.js')
 
-      const realPath = path.resolve(__dirname, '..', 'FileSystem.js');
-      const fakePath = path.resolve(__dirname, '..', 'fake.js');
-
-      expect(fileExists(realPath)).to.equal(true);
-      expect(fileExists(fakePath)).to.equal(false);
-
-    });
-  });
+      expect(fileExists(realPath)).toEqual(true)
+      expect(fileExists(fakePath)).toEqual(false)
+    })
+  })
 
   describe('readDir', () => {
-
     it('Sould read a directory', () => {
-
-      const dir = path.resolve(__dirname, '..', '..', 'client');
-      const files = readDir(dir);
-      expect(files.length).to.equal(1);
-
-    });
-  });
+      const dir = path.resolve(__dirname, '..', '..', 'client')
+      const files = readDir(dir)
+      expect(files.length).toEqual(1)
+    })
+  })
 
   describe('readFile', () => {
-
     it('Sould write a file', () => {
+      const file = path.resolve(__dirname, 'test.js')
+      const content = 'Hello!'
+      writeFile(file, content)
 
-      const file = path.resolve(__dirname, 'test.js');
-      const content = 'Hello!';
-      writeFile(file,content);
+      const readed = readFile(file, 'utf8')
 
-      const readed = readFile(file, 'utf8');
-
-      fs.unlinkSync(file);
-      expect(readed).to.equal(content);
-
-    });
-  });
+      fs.unlinkSync(file)
+      expect(readed).toEqual(content)
+    })
+  })
 
   describe('writeFile', () => {
-
     it('Sould write a file', () => {
+      const file = path.resolve(__dirname, 'test.js')
+      const content = 'Hello!'
+      writeFile(file, content)
 
-      const file = path.resolve(__dirname, 'test.js');
-      const content = 'Hello!';
-      writeFile(file,content);
+      const wrote = fs.readFileSync(file, 'utf8')
+      fs.unlinkSync(file)
 
-      const wrote  = fs.readFileSync(file, "utf8");
-      fs.unlinkSync(file);
-
-      expect(wrote).to.equal(content);
-
-    });
-  });
-
-});
+      expect(wrote).toEqual(content)
+    })
+  })
+})
--- src/base/shared/spec/TypeHelper.shared.spec.js
+++ src/base/shared/spec/TypeHelper.shared.spec.js
@@ -1,4 +1,3 @@
-import { expect } from 'chai'
 import { typeBuilder, generateFetchTypes } from '../TypeHelper'
 
 describe('shared / type-helper', () => {
@@ -7,8 +6,8 @@ describe('shared / type-helper', () => {
       let TypeExamples = ['typeExample', 'typeExample2']
       let types = typeBuilder(TypeExamples)
 
-      expect(types.typeExample).to.equal('typeExample')
-      expect(types.typeExample2).to.equal('typeExample2')
+      expect(types.typeExample).toEqual('typeExample')
+      expect(types.typeExample2).toEqual('typeExample2')
     })
   })
 
@@ -17,10 +16,10 @@ describe('shared / type-helper', () => {
       let TypeExamples = 'TEST'
       let types = generateFetchTypes(TypeExamples)
 
-      expect(types.length).to.equal(3)
-      expect(types[0]).to.equal('TEST_REQUEST')
-      expect(types[1]).to.equal('TEST_SUCCESS')
-      expect(types[2]).to.equal('TEST_ERROR')
+      expect(types.length).toEqual(3)
+      expect(types[0]).toEqual('TEST_REQUEST')
+      expect(types[1]).toEqual('TEST_SUCCESS')
+      expect(types[2]).toEqual('TEST_ERROR')
     })
   })
 })
